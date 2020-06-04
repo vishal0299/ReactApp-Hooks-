f14ad7864f72c3bd5f461abfbe7bbfe9d56e1957
vizzzzz <vishal21999@gmail.com>
2020-06-05 01:22:44 +0530

    Add func.

--- src/forms/AddUserForm.js
+++ src/forms/AddUserForm.js
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 
 const AddUserForm = props => {
+
+    const initialFormState = {id: null, name: "", username: "" }
+    const [user, setUser] = useState(initialFormState)
+
+    const handleInputChange = event =>{
+        const { name, value } = event.target
+        setUser({ ...user,[name]: value })
+    };
+
   return (
     <form>
       <label>Name</label>
-      <input type="text" name="name" value="" />
+      <input type="text" name="name" value={user.name} onChange={handleInputChange} />
       <label>Username</label>
-      <input type="text" name="username" value="" />
+      <input type="text" name="username" value={user.username} onChange={handleInputChange} />
       <button>Add new user</button>
     </form>
   )
